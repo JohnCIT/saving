fe78fa04a1cc1de5ec9fdd05dde7f9aaadc3c4b6
John <john.murphy7@mycit.ie>
2013-07-25 14:02:11 +0100

    MainState: Set up class

--- src/state/MainState.java
+++ src/state/MainState.java
@@ -1,5 +1,9 @@
 package state;
 
+import java.math.BigDecimal;
+
+import org.joda.time.DateTime;
+
 /**
  * Stores the main state of the program
  * @author john
@@ -8,5 +12,104 @@ package state;
  */
 
 public class MainState {
+	private DateTime 	beginDate;
+	private DateTime 	endDate;
+	private BigDecimal 	startingAmount;
+	private BigDecimal	goalAmount;
+	private int 		paymentPeriod;
+	
+	
+	Object[][] tableContents;
+	
+	
+	public MainState(){}
+	
+	public MainState(DateTime beginDate, DateTime endDate, BigDecimal startingAmount, BigDecimal goalAmount, int paymentPeriod, Object[][] tableContents)
+	{
+		this.beginDate 		= beginDate;
+		this.endDate		= endDate;
+		this.startingAmount = startingAmount;
+		this.goalAmount 	= goalAmount;
+		this.paymentPeriod 	= paymentPeriod;
+		this.tableContents 	= tableContents;
+	}
+
+	
+
+	//Setters
+	public void setBeginDate(DateTime beginDate) 
+	{
+		this.beginDate = beginDate;
+	}
+
+	
+
+	public void setEndDate(DateTime endDate) 
+	{
+		this.endDate = endDate;
+	}
+
+	
+	public void setStartingAmount(BigDecimal startingAmount) 
+	{
+		this.startingAmount = startingAmount;
+	}
+
+	
+
+	public void setGoalAmount(BigDecimal goalAmount) 
+	{
+		this.goalAmount = goalAmount;
+	}
+
+	public void setTableContents(Object[][] tableContents)
+	{
+		this.tableContents = tableContents;
+	}
+	
+
+
+	public void setPaymentPeriod(int paymentPeriod) 
+	{
+		this.paymentPeriod = paymentPeriod;
+	}
+
+	
+	//Getters
+	public DateTime getBeginDate() 
+	{
+		return beginDate;
+	}
+	
+	public DateTime getEndDate() 
+	{
+		return endDate;
+	}
+	
+	
+	public BigDecimal getStartingAmount() 
+	{
+		return startingAmount;
+	}
+	
+	
+	
+	public BigDecimal getGoalAmount() 
+	{
+		return goalAmount;
+	}
+	
+	public Object[][] getTableContents() 
+	{
+		return tableContents;
+	}
+	
+	public int getPaymentPeriod() 
+	{
+		return paymentPeriod;
+	}
+
+	
+	
 
 }
