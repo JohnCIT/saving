0b4a061339b31604eb8e300518779153b73a435b
John <john.murphy7@mycit.ie>
2013-07-25 16:03:25 +0100

    Model: Added method getAverage

--- src/logic/Model.java
+++ src/logic/Model.java
@@ -1,4 +1,6 @@
 package logic;
+import java.math.BigDecimal;
+
 import org.joda.time.DateTime;
 
 
@@ -41,6 +43,21 @@ public class Model {
 	{
 		int weeks = (int) (daysBetween/7);
 		return weeks;
+	}
+	
+	
+	/**
+	 * Gets the average needed to meet the goal
+	 * @param weeks
+	 * @param startingAmount
+	 * @param goal
+	 * @return
+	 */
+	public static BigDecimal averageAmountWeekly(int weeks, BigDecimal startingAmount, BigDecimal goal)
+	{
+		BigDecimal average = goal.subtract(startingAmount);
+		average = average.divide(new BigDecimal(weeks));
+		return average;
 	}
 
 }
