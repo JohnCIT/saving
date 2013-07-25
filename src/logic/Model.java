3a899de13fab21a33f574adf532d2f6ba8d76d9a
John <john.murphy7@mycit.ie>
2013-07-25 13:04:00 +0100

    Model: Added how many weeks between dates

--- src/logic/Model.java
+++ src/logic/Model.java
@@ -1,5 +1,9 @@
 package logic;
 
+import java.util.Calendar;
+import java.util.Date;
+
+
 /**
  * @author john
  * version 1
@@ -13,11 +17,30 @@ public class Model {
 	
 	/**
 	 * Headings for the table
+	 * @return The headings for the table
 	 */
 	public String[] getSaveTableHeadings()
 	{
 		String[] headings = {"Expected", "Have"};
 		return headings;
+	}
+	
+	/**
+	 * Works out how many weeks between begin date and the end date
+	 * @param beginDate
+	 * @param endDate
+	 * @return
+	 */
+	public long howManyWeeksToReachGoalAmount(Date beginDate, Date endDate)
+	{		
+		Calendar begin = Calendar.getInstance();begin.setTime(beginDate); 
+		long daysBetween = 0;  
+		while (begin.before(endDate)) 
+		{  
+			begin.add(Calendar.DAY_OF_MONTH, 1);  
+			daysBetween++;  
+		}  
+		return daysBetween/7;  
 	}
 
 }
