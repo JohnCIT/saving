2e0c4e018a42b16b40d901e551bb6a40c6e5df23
John <john.murphy7@mycit.ie>
2013-07-27 12:25:22 +0100

    Storage: Added serialize functionality

--- src/Storage.java
+++ src/Storage.java
@@ -1,3 +1,9 @@
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
 import state.MainState;
 
 /**
@@ -13,15 +19,41 @@ public class Storage {
 	 */
 	public static MainState loadState()
 	{
-		MainState state = new MainState();;
-		
+		MainState state = new MainState();
+		try
+	      {
+	         FileInputStream fileIn = new FileInputStream("Budget.txt");
+	         ObjectInputStream in   = new ObjectInputStream(fileIn);
+	         state                	=  (MainState) in.readObject();
+	         in.close();
+	         fileIn.close();
+	      }catch(IOException i)
+	      {
+	         i.printStackTrace();
+	       
+	      }catch(ClassNotFoundException c)
+	      {
+	         c.printStackTrace();
+	       
+	      }
+	
 		return state;
 	}
 	
 	
-	public static void saveState()
+	public static void saveState(MainState state)
 	{
-		
+		try
+	      {
+	         FileOutputStream fileOut = new FileOutputStream("Budget.txt");
+	         ObjectOutputStream out   = new ObjectOutputStream(fileOut);
+	         out.writeObject(state);
+	         out.close();
+	         fileOut.close();
+	      }catch(IOException i)
+	      {
+	          i.printStackTrace();
+	      }
 	}
 
 }
