3df5bc45b95a4ee86046ec639682d8286c71efdd
John <john.murphy7@mycit.ie>
2013-07-29 14:33:37 +0100

    TableDataConversion: Added new method to save to the userHave Array

--- src/utill/TableDataConversion.java
+++ src/utill/TableDataConversion.java
@@ -9,6 +9,8 @@ import java.math.BigDecimal;
 
 import javax.swing.table.DefaultTableModel;
 
+import state.MainState;
+
 public class TableDataConversion {
 	
 	/**
@@ -39,6 +41,32 @@ public class TableDataConversion {
 		for (int i = 0 ; i < nRow ; i++)
 			for (int j = 0 ; j < nCol ; j++)
 				tableData[i] = (BigDecimal) dtm.getValueAt(i,j);
+		return tableData;
+	}
+	
+	
+	/**
+	 * Convert the table data model into a 2D array and supplys the have array in state with what the user typed in
+	 * @param dtm Data table model
+	 * @return 2D array
+	 */
+	public static Object[][] getTableDataAsArrayandSaveToHave(DefaultTableModel dtm, MainState state)
+	{
+		int nRow = dtm.getRowCount(), nCol = dtm.getColumnCount();
+		Object[][] tableData = new Object[nRow][nCol];
+		for (int i = 0 ; i < nRow ; i++)
+			for (int j = 0 ; j < nCol ; j++)
+			{
+				tableData[i][j] = dtm.getValueAt(i,j);
+				if(j == 2)
+				{
+					String strval = String.valueOf(tableData[i][j]) ;
+					int val = Integer.parseInt(strval);
+					state.userHave.add(i, new BigDecimal(val) );
+				}
+				
+			}
+				
 		return tableData;
 	}
 	
